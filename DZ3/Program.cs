0f44afcfe52cbef456ba078f171d361a79c99df0
Mihail <Dante9041@mail.ru>
2022-06-10 10:56:01 +0300

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write(" vedi shislo: ");
+string? input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int result=number%2;
+if (result>0)
+{
+    Console.WriteLine("Нечетное");
+}
+else
+{
+    Console.WriteLine("четное");
+}
