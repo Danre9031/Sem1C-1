2655ab05da7b424ef25ff86963511191f06e1857
Mihail <Dante9041@mail.ru>
2022-06-10 10:55:27 +0300

    add DZ2

---
+++ DZ2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write(" vedi shislo1: ");
+string? input = Console.ReadLine();
+
+Console.Write(" vedi shislo2: ");
+string? input2 = Console.ReadLine();
+
+Console.Write(" vedi shislo3: ");
+string? input3 = Console.ReadLine();
+
+int number = Convert.ToInt32(input);
+int number2 = Convert.ToInt32(input2);
+int number3 = Convert.ToInt32(input3);
+
+if (number > number2 && number > number3)
+{
+    Console.WriteLine("max= " + number);
+}
+else if (number2 > number3 && number2 > number)
+{
+    Console.WriteLine("max= " + number2);
+}
+else
+{
+    Console.WriteLine("max= " + number3);
+}
