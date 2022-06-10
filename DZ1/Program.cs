77c171aa8098fc08f3c94ef81449dfee9e6785ac
Mihail <Dante9041@mail.ru>
2022-06-10 10:54:53 +0300

    add DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write(" vedi shislo1: ");
+string? input = Console.ReadLine();
+
+Console.Write(" vedi shislo2: ");
+string? input2 = Console.ReadLine();
+
+int number = Convert.ToInt32(input);
+int number2 = Convert.ToInt32(input2);
+
+if (number2 > number)
+{
+    Console.WriteLine("max= " + number2);
+}
+else
+{
+    Console.WriteLine("max= " + number);
+}
