52f0176cf8e38c2c2831a9b03c8fc74c745c3074
Mihail <Dante9041@mail.ru>
2022-06-10 10:56:39 +0300

    add DZ4

---
+++ DZ4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write(" vedi shislo: ");
+string? input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+//Console.WriteLine(input);
+int number2 =1;
+int number3;
+
+while (number>number2)
+{
+    number3 =number2%2;
+if (number3>0)
+{
+    number2++;
+}
+else
+{
+    Console.WriteLine(number2+" ");
+    number2++;
+}
+}
